1ad3dcd81c7b7edf02d0c6202b03f581dc866a25
Yury Botov <yury.botov@gmail.com>
2019-07-10 12:26:11 +0300

    Add banners

--- src/Octoliner.cpp
+++ src/Octoliner.cpp
@@ -1,3 +1,14 @@
+/*
+ * This file is a part of Dragster car set library
+ *
+ * Realise: octoliner object - 8-point line-folowing sensor driver
+ * © Amperka LLC (https://amperka.com, dev@amperka.com)
+ * 
+ * Author: Vasily Basalaev <vasily@amperka.ru>
+ * Refactored by: Yury Botov <by@amperka.com>
+ * License: GPLv3, all text here must be included in any redistribution.
+ */
+
 #include "Octoliner.h"
 #include <Wire.h>
 
