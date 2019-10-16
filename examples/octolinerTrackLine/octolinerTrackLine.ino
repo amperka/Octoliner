6c43d0c4444331fe5b3f6370dcee75661294f81f
yurybotov <yury.botov@gmail.com>
2019-10-16 12:57:18 +0300

    made correction to octolinerMapLine.ino
    mapLine deprecated, rename file and catalog to octolinerTrackLine

--- examples/octolinerTrackLine/octolinerTrackLine.ino
+++ examples/octolinerTrackLine/octolinerTrackLine.ino
@@ -1,5 +1,3 @@
-// библиотека для работы с модулями I²C
-#include <Wire.h>
 // библиотека для работы cо сборкой датчиков линии
 #include <Octoliner.h>
 
@@ -8,29 +6,24 @@ Octoliner octoliner(42);
 
 void setup() {
   // открываем Serial-порт
-  Serial.begin(115200);
-  // начало работы с модулями I²C
-  Wire.begin();
+  Serial.begin(115200);;
   // начало работы с датчиками линии
   octoliner.begin();
   // выставляем чувствительность фотоприёмников в диапазоне от 0 до 255
-  octoliner.setSensitivity(200);
-  // выставляем яркость свечения ИК-светодиодов в диапазоне от 0 до 255
-  octoliner.setBrightness(255);
-
+  octoliner.setSensitivity(208);
 }
 
 void loop() {
   // массив для хранения показателей с датчиков линии
-  int dataSensors[8];
+  int16_t dataSensors[8];
   // считываем значение с датчиков линии
-  for (int i = 0; i < 8; i++) {
+  for (uint8_t i = 0; i < 8; i++) {
     // выводим значения в Serial-порт
     dataSensors[i] = octoliner.analogRead(i);
-    Serial.print(octoliner.analogRead(i));
+    Serial.print(dataSensors[i]);
     Serial.print("\t");
   }
-  Serial.println(octoliner.mapLine(dataSensors));
+  Serial.println(octoliner.trackLine(dataSensors));
   Serial.println();
   // ждём пол секунды
   delay(500);
