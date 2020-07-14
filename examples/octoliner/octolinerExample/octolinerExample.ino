5eac8d3997419947c553b679804c38010b37df32
Yury Botov <yury.botov@gmail.com>
2020-07-14 09:32:19 +0300

    auto-prettifyer

--- examples/octoliner/octolinerExample/octolinerExample.ino
+++ examples/octoliner/octolinerExample/octolinerExample.ino
@@ -7,26 +7,26 @@
 Octoliner octoliner(42);
 
 void setup() {
-  // открываем Serial-порт
-  Serial.begin(115200);
-  // начало работы с модулями I²C
-  Wire.begin();
-  // начало работы с датчиками линии
-  octoliner.begin();
-  // выставляем чувствительность фотоприёмников в диапазоне от 0 до 255
-  octoliner.setSensitivity(200);
-  // выставляем яркость свечения ИК-светодиодов в диапазоне от 0 до 255
-  octoliner.setBrightness(255);
+    // открываем Serial-порт
+    Serial.begin(115200);
+    // начало работы с модулями I²C
+    Wire.begin();
+    // начало работы с датчиками линии
+    octoliner.begin();
+    // выставляем чувствительность фотоприёмников в диапазоне от 0 до 255
+    octoliner.setSensitivity(200);
+    // выставляем яркость свечения ИК-светодиодов в диапазоне от 0 до 255
+    octoliner.setBrightness(255);
 }
 
 void loop() {
-  // считываем значение с датчиков линии
-  for (int i = 0; i < 8; i++) {
-    // выводим значения в Serial-порт
-    Serial.print(octoliner.analogRead(i));
-    Serial.print("\t");
-  }
-  Serial.println();
-  // ждём пол секунды
-  delay(500);
+    // считываем значение с датчиков линии
+    for (int i = 0; i < 8; i++) {
+        // выводим значения в Serial-порт
+        Serial.print(octoliner.analogRead(i));
+        Serial.print("\t");
+    }
+    Serial.println();
+    // ждём пол секунды
+    delay(500);
 }
